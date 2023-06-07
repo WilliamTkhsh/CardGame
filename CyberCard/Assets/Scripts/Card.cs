c00d40cd489c43b277e955b784eb5c3c48c8b3db
William Takahashi <>
2023-06-06 21:04:25 -0300

    Criação de scripts e exportação de blender

---
+++ CyberCard/Assets/Scripts/Card.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+
+    //TODO: Colocar aqui todas as propriedades da carta
+    public static int attack;
+    public static int defense;
+}
---
+++ CyberCard/Assets/Scripts/CardEditor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardEditor : MonoBehaviour
+{
+
+    public void SetName(string name)
+    {
+        Debug.Log(name);
+    }
+
+    public void SetType(int typeIndex)
+    {
+        Debug.Log(typeIndex);
+    }
+
+    public void SetMaterial(int materialIndex)
+    {
+
+    }
+
+    public void SetAttack(int attack)
+    {
+
+    }
+
+    public void SetDefense(int defense)
+    {
+
+    }
+
+    public void SaveCard()
+    {
+
+    }
+}
